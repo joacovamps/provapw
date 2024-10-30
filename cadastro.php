f7ebab4308f5e4c54491f211ae41100827e6e684
Joaquim <kkimnetto@gmail.com>
2024-10-30 08:09:32 -0300

    Botão de Cadastro

--- cadastro.php
+++ cadastro.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Prova</title>
+    <title>Cadastro</title>
 </head>
 <body>
-    <h1> Joaquim Martins Neto, Monalisa Nascimento de Araújo </h1>
-    <img src="imagens/pizza.png" alt="Pizza">
+    <button onclick="window.location.href='cadastro.php' ">Ir para cadastros</button>
 </body>
 </html>
